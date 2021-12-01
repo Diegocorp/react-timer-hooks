7c7239a0ed07972d276c665e679509042c69d535
Diegocorp <diegosandovalr.corp@gmail.com>
2021-12-01 10:35:22 -0700

    second method

--- src/timer.jsx
+++ src/timer.jsx
@@ -2,50 +2,54 @@ import React, { useState, useEffect } from "react";
 import "./timer.css";
 
 const Timer = () => {
-  const [second, setSecond] = useState("0");
-  const [minute, setMinute] = useState("0");
+  const [time, setTime] = useState({
+    seconds: "0",
+    minutes: "0",
+    hours: "0",
+  });
   const [isActive, setIsActive] = useState(false);
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    let intervalId;
-
+    let interval = null;
     if (isActive) {
-      intervalId = setInterval(() => {
-        const secondCounter = counter % 60;
-        const minuteCounter = Math.floor(counter / 60);
-
-        setSecond(secondCounter);
-        setMinute(minuteCounter);
+      interval = setInterval(() => {
+        setTime({
+          seconds: counter % 60,
+          minutes: Math.floor(counter / 60) % 60,
+          hours: Math.floor(counter / 3600),
+        });
 
         setCounter((counter) => counter + 1);
       }, 1000);
     }
-
-    return () => clearInterval(intervalId);
+    return () => clearInterval(interval);
   }, [isActive, counter]);
 
-  function stopTimer() {
+  const restartTimer = () => {
     setIsActive(false);
+    setTime({
+      seconds: "0",
+      minutes: "0",
+      hours: "0",
+    });
     setCounter(0);
-    setSecond("0");
-    setMinute("0");
-  }
+  };
 
   return (
-    <div class="container">
-      <div class="time">
-        <span class="minute">{minute}</span>
+    <div className="container">
+      <div className="time">
+        <span className="minute">{time.hours}</span>
         <span>:</span>
-        <span class="second">{second}</span>
+        <span className="minute">{time.minutes}</span>
+        <span>:</span>
+        <span className="second">{time.seconds}</span>
       </div>
-      <div class="buttons">
-        <button onClick={() => setIsActive(!isActive)} class="start">
-          {isActive ? "Pause" : "Start"}
+      <div className="buttons">
+        <button onClick={() => setIsActive(!isActive)}>
+          {isActive ? "PAUSE" : "START"}
         </button>
-        <button onClick={stopTimer} class="reset">
-          Reset
-        </button>
+        <button onClick={restartTimer}>RESTART</button>
       </div>
     </div>
   );
